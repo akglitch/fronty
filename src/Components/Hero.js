df2fe4c7b416f1192e90b74487e40d9ed60c4210
kaytee <mcaninakwa@gmail.com>
2022-11-02 13:56:05 -0400

    initial commit

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,28 +1,22 @@
-import { Text, Button, Container, Grid, Image } from "@nextui-org/react"
+import { Text, Button, Container } from "@nextui-org/react"
 
 
 function Hero() {
     return (
-        <Container lg>
-            <Grid.Container>
-                <Grid sm={6} xs={12} md={6} lg={6} justify="center" css={{ paddingTop: "$14" }}>
+        <div className="background">
+           <Container>
                     {/* <p>FexxaAi<br></br> <h1>welcome to fexa ai we welcome you to the world of ml</h1></p> */}
-                    <Text h1 size={60} weight={50} css={{ textGradient: "45deg, $red600 -20%, $yellow600 50%" }}  >welcome to dome let's make the web prettier for the whole world</Text>
+                    <Text className="herotext" h1 size={35} weight={30} css={{color:"White"}} >I'm a frontend developer <br></br> with a passion for crafting elegant,  <br></br>user-friendly interfaces that are both functional and beautiful.</Text>
 
-                </Grid>
+              
 
-                <Grid sm={6} xs={12} md={6} lg={6} justify="center" css={{ paddingTop: "$18", fontFamily: "$sans" }}>
-                    <Image className="img" src={"human.png"}></Image>
-
-                </Grid>
-
-                
-                
-            </Grid.Container>
-            <Button bordered color={"primary"} auto>
+              
+       
+            <Button bordered color={"gradient"} >
                let's proceed
             </Button>
-        </Container>
+            </Container>
+        </div>
     )
 }
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,54 +1,124 @@
-import { Navbar,Link, Text } from "@nextui-org/react";
+import { Navbar, Link, Text, Avatar, Dropdown } from "@nextui-org/react";
+
 import { AcmeLogo } from "../Components/AcmeLogo";
 
 export default function Nav() {
   const collapseItems = [
-    "Features",
-    "Customers",
-    "Pricing",
-    "Company",
-    "Legal",
-    "Team",
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
     "Help & Feedback",
-    "Login",
-    "Sign Up",
+    "Log Out",
   ];
 
   return (
-  
+
       <Navbar isBordered variant="sticky">
-        <Navbar.Brand>
-          <Navbar.Toggle aria-label="toggle navigation" />
+        <Navbar.Toggle showIn="xs" />
+        <Navbar.Brand
+          css={{
+            "@xs": {
+              w: "12%",
+            },
+          }}
+        >
           <AcmeLogo />
-          <Text b color="#000000" hideIn="xs">
+          <Text b color="inherit" hideIn="xs">
             KAYTEE
           </Text>
         </Navbar.Brand>
-        <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline" >
-          <Navbar.Link href="#">ABOUT</Navbar.Link>
+        <Navbar.Content
+          enableCursorHighlight
+          activeColor="secondary"
+          hideIn="xs"
+          variant="highlight-rounded"
+        >
+          <Navbar.Link href="#">Features</Navbar.Link>
           <Navbar.Link isActive href="#">
-            PROJECTS
+            Customers
           </Navbar.Link>
-          <Navbar.Link href="#">CONTACTS</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
           <Navbar.Link href="#">Company</Navbar.Link>
         </Navbar.Content>
-      
-        <Navbar.Collapse>
-        {collapseItems.map((item, index) => (
-          <Navbar.CollapseItem key={item}>
-            <Link
-              color="inherit"
-              css={{
-                minWidth: "100%",
-              }}
-              href="#"
+        <Navbar.Content
+          css={{
+            "@xs": {
+              w: "12%",
+              jc: "flex-end",
+            },
+          }}
+        >
+          <Dropdown placement="bottom-right">
+            <Navbar.Item>
+              <Dropdown.Trigger>
+                <Avatar
+                  bordered
+                  as="button"
+                  color="secondary"
+                  size="md"
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                />
+              </Dropdown.Trigger>
+            </Navbar.Item>
+            <Dropdown.Menu
+              aria-label="User menu actions"
+              color="secondary"
+              onAction={(actionKey) => console.log({ actionKey })}
             >
-              {item}
-            </Link>
-          </Navbar.CollapseItem>
-        ))}
-      </Navbar.Collapse>
+              <Dropdown.Item key="profile" css={{ height: "$18" }}>
+                <Text b color="inherit" css={{ d: "flex" }}>
+                  Signed in as
+                </Text>
+                <Text b color="inherit" css={{ d: "flex" }}>
+                  zoey@example.com
+                </Text>
+              </Dropdown.Item>
+              <Dropdown.Item key="settings" withDivider>
+                My Settings
+              </Dropdown.Item>
+              <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
+              <Dropdown.Item key="analytics" withDivider>
+                Analytics
+              </Dropdown.Item>
+              <Dropdown.Item key="system">System</Dropdown.Item>
+              <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
+              <Dropdown.Item key="help_and_feedback" withDivider>
+                Help & Feedback
+              </Dropdown.Item>
+              <Dropdown.Item key="logout" withDivider color="error">
+                Log Out
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Content>
+        <Navbar.Collapse>
+          {collapseItems.map((item, index) => (
+            <Navbar.CollapseItem
+              key={item}
+              activeColor="secondary"
+              css={{
+                color: index === collapseItems.length - 1 ? "$error" : "",
+              }}
+              isActive={index === 2}
+            >
+              <Link
+                color="inherit"
+                css={{
+                  minWidth: "100%",
+                }}
+                href="#"
+              >
+                {item}
+              </Link>
+            </Navbar.CollapseItem>
+          ))}
+        </Navbar.Collapse>
       </Navbar>
-
-  )
+   
+  );
 }
